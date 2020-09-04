57a07fef60e03904e391abe671b32d10f0f31a01
Sloan <sloangwaltney2@gmail.com>
2020-09-03 21:58:53 -0400

    added user router

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const { connect } = require("mongoose");
-
+const { userRouter } = require('./router/userRouter')
 async function run() {
   try {
     await connect(
@@ -11,6 +11,7 @@ async function run() {
     const app = express();
 
     app.use(express.json());
+    app.use("/api/v1/users", userRouter)
 
     app.listen(process.env.PORT, () => console.log("GameRate API Running"));
   } catch (e) {
