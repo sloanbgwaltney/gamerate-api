416e498224ad740bfe5afb4129dd36bc71d9bf48
Sloan <sloangwaltney2@gmail.com>
2020-09-04 01:09:51 -0400

    created some methods for the user

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const { hash, compare } = require('bcryptjs');
 
 const userSchema = new Schema({
   username: {
@@ -13,6 +14,29 @@ const userSchema = new Schema({
     type: String,
   },
 });
+
+userSchema.methods.hashPassword = async function () {
+  this.password = await hash(this.password, 10)
+}
+
+userSchema.methods.isUnqiueUser = async function () {
+  const results = await this.model("user").find({ $or: [{ username: this.username }, { email: this.email }] })
+  return results.length === 0
+}
+
+userSchema.methods.saveAndReturnSanitized = async function () {
+  const savedUser = await this.save()
+  savedUser.password = undefined
+  return savedUser
+}
+
+userSchema.methods.validPassword = async function (plainPassword) {
+  return compare(plainPassword, this.password)
+}
+
+userSchema.statics.findOneByUsername = async function (username) {
+  return this.findOne({ username })
+}
 
 const User = model("user", userSchema);
 
