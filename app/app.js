389a85a793558e3f113223b73e9dbaec43274f81
Sloan <sloangwaltney2@gmail.com>
2020-09-04 01:09:27 -0400

    added security controller

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const { connect } = require("mongoose");
 const { userRouter } = require('./router/userRouter')
+const { securityRouter } = require('./router/securityRouter')
+
 async function run() {
   try {
     await connect(
@@ -12,7 +14,7 @@ async function run() {
 
     app.use(express.json());
     app.use("/api/v1/users", userRouter)
-
+    app.use("/api/v1/security", securityRouter)
     app.listen(process.env.PORT, () => console.log("GameRate API Running"));
   } catch (e) {
     console.log(e);
