88ef458c166f92eb722a1fa1871b8e51c49287e3
Sloan <sloangwaltney2@gmail.com>
2020-09-05 23:41:23 -0400

    added tokens

--- app/controllers/securityController.js
+++ app/controllers/securityController.js
@@ -1,5 +1,6 @@
 const { User } = require('../models/user')
 const createError = require('http-errors')
+const { createSLJWT, createRefreshJWT } = require('../lib/token')
 
 function login() {
     return async function (req, res, next) {
@@ -7,7 +8,9 @@ function login() {
             const requestor = await User.findOneByUsername(req.body.username)
             if (!requestor) return next(createError(400, 'Invalid Username or Password'))
             if (! await requestor.validPassword(req.body.password)) return next(createError(400, 'Invalid Username or Password'))
-            // do jwt magic
+            const accessToken = await createSLJWT(requestor.id)
+            const refreshToken = await createRefreshJWT(requestor.id)
+            res.status(200).json({ accessToken, refreshToken })
         } catch (e) {
             console.log(e)
             next(createError(500))
