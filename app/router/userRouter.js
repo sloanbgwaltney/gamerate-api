40bc160a6c0d6e14be6e3c6f651cbe960b84a811
Sloan <sloangwaltney2@gmail.com>
2020-09-08 04:19:53 -0400

    added created by query endpoint for creation user

--- app/router/userRouter.js
+++ app/router/userRouter.js
@@ -1,8 +1,9 @@
 const userRouter = require('express').Router()
-const { createUser } = require('../controllers/userController')
+const { createUser, getGameProfilesCreatedByUser } = require('../controllers/userController')
 const { validateBody } = require('../lib/validateBody')
 const { createUserValidator } = require('../validators/userValidators')
+const { passport } = require('../config/passport')
 
 userRouter.post("", validateBody(createUserValidator), createUser())
-
+userRouter.get("/createdGameProfiles", passport.authenticate('jwt', { session: false }), getGameProfilesCreatedByUser())
 module.exports = { userRouter }
