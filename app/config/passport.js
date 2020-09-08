2f7e97e2272b21979d1829e3dad545c164ff6884
Sloan <sloangwaltney2@gmail.com>
2020-09-08 09:36:59 -0400

    some custom passport

--- app/config/passport.js
+++ app/config/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const { User } = require('../models/user')
 const JwtStrategy = require('passport-jwt').Strategy
+const CustomStrategy = require('passport-custom').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const createError = require('http-errors')
 
@@ -31,5 +32,19 @@ passport.use(new JwtStrategy(opts, async function (payload, done) {
         done(e)
     }
 }))
+
+passport.use('level-3-gameprofile-access', new CustomStrategy(passportGameProfileAccessSetup(3)))
+passport.use('level-2-gameprofile-access', new CustomStrategy(passportGameProfileAccessSetup(2)))
+passport.use('level-1-gameprofile-access', new CustomStrategy(passportGameProfileAccessSetup(1)))
+
+function passportGameProfileAccessSetup(level) {
+    return function (request, done) {
+        if (!request.user) throw new TypeError('User is required at this point')
+        if (!request.gameprofile) throw new TypeError('GameProfile refernce is not in the request')
+        const userAccess = request.gameprofile.getUserAccess(request.user.id)
+        if (userAccess.accessLevel !== level) return done(createError(403, 'You do not have the permission required for this request'), null)
+        return done(null, request.user)
+    }
+}
 
 module.exports = { passport }
