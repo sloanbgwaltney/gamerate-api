bf0312efacca5d1266e098e56dd0ee88105aae98
Sloan <sloangwaltney2@gmail.com>
2020-09-08 05:17:56 -0400

    added a createPerformaceCategory method

--- app/controllers/gameProfileController.js
+++ app/controllers/gameProfileController.js
@@ -14,4 +14,16 @@ function createGameProfile() {
     }
 }
 
-module.exports = { createGameProfile }
+function createPerformanceCategory() {
+    return async function (req, res, next) {
+        try {
+            req.gameProfile.createPerformanceCategory(req.body, req.user.id)
+            const gameProfile = await req.gameProfile.save()
+            res.status(201).json(gameProfile)
+            next()
+        } catch (e) {
+            next(createError(500))
+        }
+    }
+}
+module.exports = { createGameProfile, createPerformanceCategory }
