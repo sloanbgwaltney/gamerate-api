57272eb85d92e16bdd48c59417e3279d19373af3
Sloan <sloangwaltney2@gmail.com>
2020-09-08 05:18:21 -0400

    added post endpoint to create performance category

--- app/router/gameProfileRouter.js
+++ app/router/gameProfileRouter.js
@@ -1,7 +1,17 @@
 const gameProfileRouter = require('express').Router()
-const { createGameProfile } = require('../controllers/gameProfileController')
+const { createGameProfile, createPerformanceCategory } = require('../controllers/gameProfileController')
 const { passport } = require('../config/passport')
+const { fetchEntity } = require('../lib/fetchEntity')
+const { GameProfile } = require('../models/gameProfile')
+const { validateBody } = require('../lib/validateBody')
+const { createPerformanceCategorySchema } = require('../validators/performanceCategoryValidators')
 
 gameProfileRouter.post('', passport.authenticate('jwt', { session: false }), createGameProfile())
-
+gameProfileRouter.post(
+    '/:gameProfileId/performanceCategory',
+    passport.authenticate('jwt', { session: false }),
+    fetchEntity(GameProfile, 'gameProfileId', 'gameProfile'),
+    validateBody(createPerformanceCategorySchema),
+    createPerformanceCategory()
+)
 module.exports = { gameProfileRouter }
