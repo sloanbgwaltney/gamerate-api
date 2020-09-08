3a65c520af190878d13bd87cd2b40eacaf1a75f7
Sloan <sloangwaltney2@gmail.com>
2020-09-08 04:01:09 -0400

    app now uses game profile router

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const { connect } = require("mongoose");
 const passport = require('passport')
 const { userRouter } = require('./router/userRouter')
 const { securityRouter } = require('./router/securityRouter')
-const { validateRequestor } = require('./lib/validateRequestor')
+const { gameProfileRouter } = require('./router/gameProfileRouter')
 
 async function run() {
   try {
@@ -15,10 +15,11 @@ async function run() {
     const app = express();
     app.use(passport.initialize())
     app.use(express.json());
-    app.use(validateRequestor());
 
     app.use("/api/v1/users", userRouter)
     app.use("/api/v1/security", securityRouter)
+    app.use("/api/v1/gameProfile", gameProfileRouter)
+
     app.listen(process.env.PORT, () => console.log("GameRate API Running"));
   } catch (e) {
     console.log(e);
