a32bcef74e19bb40b8c2d361ae23423ccfd3f83d
Sloan <sloangwaltney2@gmail.com>
2020-09-08 05:17:39 -0400

    added method for creation of perfromance category

--- app/models/gameProfile.js
+++ app/models/gameProfile.js
@@ -29,12 +29,21 @@ gameProfileSchema.methods.create = async function (userId) {
     this.createdDate = new Date()
     this.createdBy = userId
     this.lastUpdatedBy = userId
-    this.lastUpdatedDate = new Date()
+    this.lastUpdatedDate = this.createdDate
     return this.save()
 }
 
 gameProfileSchema.statics.getByCreationUser = async function (userId) {
     return this.model('gameprofile').find({ createdBy: userId })
+}
+
+gameProfileSchema.methods.createPerformanceCategory = async function (pc, userId) {
+    pc.createdDate = new Date()
+    pc.createdBy = userId
+    pc.lastUpdatedBy = userId
+    pc.lastUpdatedDate = pc.createdDate
+    if (!Array.isArray(this.performanceCategories)) { this.performanceCategories = [] }
+    this.performanceCategories.push(pc)
 }
 
 const GameProfile = model('gameprofile', gameProfileSchema)
