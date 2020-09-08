7802cd51a9fbd1af1a38c35f1658478c7d0fe971
Sloan <sloangwaltney2@gmail.com>
2020-09-07 23:20:07 -0400

    post passport clean up

--- app/controllers/securityController.js
+++ app/controllers/securityController.js
@@ -1,16 +1,11 @@
-const { User } = require('../models/user')
 const createError = require('http-errors')
-const { createSLJWT, createRefreshJWT } = require('../lib/token')
+const { createJWT } = require('../lib/token')
 
 function login() {
     return async function (req, res, next) {
         try {
-            const requestor = await User.findOneByUsername(req.body.username)
-            if (!requestor) return next(createError(400, 'Invalid Username or Password'))
-            if (! await requestor.validPassword(req.body.password)) return next(createError(400, 'Invalid Username or Password'))
-            const accessToken = await createSLJWT(requestor.id)
-            const refreshToken = await createRefreshJWT(requestor.id)
-            res.status(200).json({ accessToken, refreshToken })
+            const accessToken = await createJWT(req.user.id)
+            res.status(200).json({ accessToken })
         } catch (e) {
             console.log(e)
             next(createError(500))
@@ -18,19 +13,19 @@ function login() {
     }
 }
 
-function refresh() {
-    return async function (req, res, next) {
-        try {
-            if (!req.usedRefreshToken) return next(createError(400, 'Invalid refresh token'))
-            const accessToken = await createSLJWT(req.user.id)
-            res.status(200).json({ accessToken })
-        } catch (e) {
-            console.log(e)
-            if (e instanceof JsonWebTokenError) return next(createError(400, 'Invalid refresh token'))
-            if (e instanceof TokenExpiredError) return next(createError(440, 'Session has expired'))
-            return next(createError(500))
-        }
-    }
-}
+// function refresh() {
+//     return async function (req, res, next) {
+//         try {
+//             if (!req.usedRefreshToken) return next(createError(400, 'Invalid refresh token'))
+//             const accessToken = await createSLJWT(req.user.id)
+//             res.status(200).json({ accessToken })
+//         } catch (e) {
+//             console.log(e)
+//             if (e instanceof JsonWebTokenError) return next(createError(400, 'Invalid refresh token'))
+//             if (e instanceof TokenExpiredError) return next(createError(440, 'Session has expired'))
+//             return next(createError(500))
+//         }
+//     }
+// }
 
 module.exports = { login, refresh }
