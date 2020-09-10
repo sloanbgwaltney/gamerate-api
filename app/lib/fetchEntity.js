67a5d0f471f0633aca4dcaea2889a29978aef845
Sloan <sloangwaltney2@gmail.com>
2020-09-10 01:45:09 -0400

    made this more robust by providing custom access to keys

--- app/lib/fetchEntity.js
+++ app/lib/fetchEntity.js
@@ -1,17 +1,12 @@
 const createError = require('http-errors')
 
-function fetchEntity(model, idKey, requestKeyToPopulate, opts = {}) {
-    const defaultOpts = {
-        errorOnNull: true
-    }
-
-    const finalOpts = Object.assign(defaultOpts, opts)
+function fetchEntity(model, method, argumentsRetriever, propertySetter) {
 
     return async function (req, res, next) {
         try {
-            const entity = await model.findById(req.params[idKey])
-            if (!entity && finalOpts.errorOnNull) return next(createError(422, `Invalid ID provided: ${req.params[idKey]}`))
-            req[requestKeyToPopulate] = entity
+            const arguments = await argumentsRetriever(req, res)
+            const entity = model[method](arguments)
+            await propertySetter(req, res, entity)
             next()
         } catch (e) {
             console.log(e)
