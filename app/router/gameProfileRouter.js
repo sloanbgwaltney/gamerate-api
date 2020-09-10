2da2104a9ddfa97a5f07b7053bcac75ac09a3cdf
Sloan <sloangwaltney2@gmail.com>
2020-09-10 02:02:50 -0400

    removed passport function

--- app/router/gameProfileRouter.js
+++ app/router/gameProfileRouter.js
@@ -5,13 +5,16 @@ const { fetchEntity } = require('../lib/fetchEntity')
 const { GameProfile } = require('../models/gameProfile')
 const { validateBody } = require('../lib/validateBody')
 const { createPerformanceCategorySchema } = require('../validators/performanceCategoryValidators')
+const { isAuthorized } = require('../lib/isAuthorized')
 
 gameProfileRouter.post('', passport.authenticate('jwt', { session: false }), createGameProfile())
 gameProfileRouter.post(
     '/:gameProfileId/performanceCategory',
     passport.authenticate(PASSPORT_KEYS.JWT, { session: false }),
     fetchEntity(GameProfile, req => req.params.gameProfileId, (req, res, entity) => req.entities.gameProfile = entity),
-    passport.authenticate(PASSPORT_KEYS.GAME_PRFILE.LEVEL2, { session: false }),
+    isAuthorized((req, res) => {
+        if (!req.entities.gameProfile) return false; return req.entities.gameProfile.hasAtLeastLevelAccess(2, req.user.id)
+    }, 'Must have level 2 acces to this game profile to create a performance category'),
     validateBody(createPerformanceCategorySchema),
     createPerformanceCategory()
 )
