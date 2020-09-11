ee7d9d4989533cd0da550773bf2b0155bb70b242
Sloan <sloangwaltney2@gmail.com>
2020-09-11 00:48:43 -0400

    added post endpoint for game profile router

--- app/router/gameProfileRouter.js
+++ app/router/gameProfileRouter.js
@@ -1,5 +1,5 @@
 const gameProfileRouter = require('express').Router()
-const { createGameProfile, createPerformanceCategory } = require('../controllers/gameProfileController')
+const { createGameProfile, createPerformanceCategory, createScoringPolicy } = require('../controllers/gameProfileController')
 const { passport, PASSPORT_KEYS } = require('../config/passport')
 const { fetchEntity } = require('../lib/fetchEntity')
 const { GameProfile } = require('../models/gameProfile')
@@ -17,5 +17,14 @@ gameProfileRouter.post(
     }, 'Must have level 2 access to this game profile to create a performance category'),
     validateBody(createPerformanceCategorySchema),
     createPerformanceCategory()
+)
+gameProfileRouter.post(
+    '/:gameProfileId/scoringPolicies',
+    passport.authenticate(PASSPORT_KEYS.JWT, { session: false }),
+    fetchEntity(GameProfile, 'findById', req => req.params.gameProfileId, (req, res, entity) => req.entities.gameProfile = entity),
+    isAuthorized((req, res) => {
+        if (!req.entities.gameProfile) return false; return req.entities.gameProfile.hasAtLeastLevelAccess(2, req.user.id)
+    }, 'Must have level 2 access to this game profile to create a scoring policy'),
+    createScoringPolicy()
 )
 module.exports = { gameProfileRouter }
