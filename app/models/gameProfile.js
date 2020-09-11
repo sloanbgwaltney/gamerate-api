3790605a66af0810b12810ea652d89105025dc51
Sloan <sloangwaltney2@gmail.com>
2020-09-11 02:37:48 -0400

    moved user acess to plugin

--- app/models/gameProfile.js
+++ app/models/gameProfile.js
@@ -1,13 +1,12 @@
 const { Schema, model } = require("mongoose");
 const { performanceCategory } = require('./performanceCategory')
-const { userAccessSchema } = require('./userAccess');
-const { UserAlreadyHasAccessLevel } = require("../errors/userAlreadyHasAccessLevel");
 const { MONGOOSE_KEYS } = require('./mongooseKeys')
 const { convertPlainObjectToMap } = require('../lib/convertPlainObjToMap')
 const { InvalidPerformanceCategoryName } = require('../errors/invalidPerformanceCategoryName')
 const { InvalidScoringTotal } = require('../errors/invalidScoringTotal')
 const { scoringPolicySchema } = require('./scoringPolicy');
 const { NameTaken } = require("../errors/nameTaken");
+const { userAccessLevelPlugin } = require('../lib/mongoosePlugins/userAccessLevelPlugin')
 
 const gameProfileSchema = new Schema({
     name: {
@@ -34,10 +33,6 @@ const gameProfileSchema = new Schema({
         type: Map,
         of: performanceCategory
     },
-    usersAccess: {
-        type: Map,
-        of: userAccessSchema
-    },
     scoringPolicies: {
         type: Map,
         of: scoringPolicySchema
@@ -53,30 +48,9 @@ gameProfileSchema.methods.create = async function (userId) {
     this.createdBy = userId
     this.lastUpdatedBy = userId
     this.lastUpdatedDate = this.createdDate
-    this.createUserAccess(userId, userId, 3)
+    console.log(this.createUserAccess)
+    this.createUserAccess(userId, 3)
     return this.save()
-}
-
-gameProfileSchema.methods.createUserAccess = async function (currentUserId, addedUserId, level) {
-    if (!this.usersAccess) this.usersAccess = new Map()
-    const addedUser = this.getUserAccess(addedUserId)
-    if (addedUser) throw new UserAlreadyHasAccessLevel(addedUser.id)
-    const date = new Date()
-    const userAccess = {
-        accessLevel: level,
-        user: currentUserId,
-        addedDate: date,
-        addeddBy: addedUserId,
-        lastUpdatedDate: date,
-        lastUpdatedBy: addedUserId
-    }
-    this.usersAccess.set(addedUserId, userAccess)
-}
-
-
-gameProfileSchema.methods.getUserAccess = function (userId) {
-    if (!this.usersAccess) return null
-    return this.usersAccess.get(userId)
 }
 
 gameProfileSchema.methods.createPerformanceCategory = async function (pc, userId) {
@@ -86,13 +60,6 @@ gameProfileSchema.methods.createPerformanceCategory = async function (pc, userId
     pc.lastUpdatedDate = pc.createdDate
     if (!this.performanceCategories) this.performanceCategories = new Map()
     this.performanceCategories.set(pc.name, pc)
-}
-
-gameProfileSchema.methods.hasAtLeastLevelAccess = function (level, userId) {
-    if (!this.usersAccess) return false
-    const userAccess = this.usersAccess.get(userId)
-    if (!userAccess) return false
-    return userAccess.accessLevel >= level
 }
 
 gameProfileSchema.methods.createScoringPolicy = function (scoringPolicy) {
@@ -113,6 +80,7 @@ gameProfileSchema.methods.validateScoringPolicy = function (scoringPolicy) {
     if (total !== 100) throw new InvalidScoringTotal(total)
 }
 
+gameProfileSchema.plugin(userAccessLevelPlugin)
 const GameProfile = model(MONGOOSE_KEYS.MODELS.GAME_PROFILE, gameProfileSchema)
 
 module.exports = { GameProfile }
