51930c7f096926f8440642d70108ffba764030ec
Sloan <sloangwaltney2@gmail.com>
2020-09-12 17:47:20 -0400

    name change

---
+++ app/models/gameResult.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose')
+const { MONGOOSE_KEYS } = require('./mongooseKeys')
+
+/**
+ * teams: {
+ *      "attacking": {
+ *          "player-123": {
+ *              "performance": {
+ *                  "performanceCategory-123": 123,
+ *                  "performanceCategory-456": 456
+ *               },
+ *               policy: "scoringPolicy-123"
+ *           }
+ *      }
+ * }
+ */
+
+const gameResultSchema = new Schema({
+    gameId: {
+        type: String,
+        unique: true
+    },
+    gameProfile: {
+        type: Schema.Types.ObjectId,
+        ref: MONGOOSE_KEYS.MODELS.GAME_PROFILE
+    },
+    gameStartedAt: Date,
+    gameEndedAt: Date,
+    teams: {
+        type: Map,
+        of: {
+            type: Map,
+            of: {
+                type: Map,
+                of: {
+                    performance: {
+                        type: Map,
+                        of: Number
+                    },
+                    policy: String
+                }
+            }
+        }
+    }
+})
+
+const GameResult = model(MONGOOSE_KEYS.MODELS.GAME_RESULT, gameResultSchema)
+
+module.exports = { GameResult }
